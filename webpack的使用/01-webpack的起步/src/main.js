94dd514e0f58ad37bc83244b62ff43370baf39b4
RJPig <1259950227@qq.com>
2020-04-25 10:18:32 +0800

    no message

--- webpack的使用/01-webpack的起步/src/main.js
+++
@@ -1,7 +0,0 @@
-// 使用commonjs的模块化规范
-const {sum} = require('./mathmul.js')
-console.log(sum(20, 30));
-
-// 使用ES6的模块化规范
-import {name, age, height} from "./info"
-console.log(name, age, height);
--- webpack的使用/02-webpack的配置/src/main.js
+++
@@ -1,7 +0,0 @@
-// 使用commonjs的模块化规范
-const {sum} = require('./js/mathmul.js')
-console.log(sum(20, 30));
-
-// 使用ES6的模块化规范
-import {name, age, height} from "./js/info"
-console.log(name, age, height);
