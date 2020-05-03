7abc0901437b9a6c78352ba96eb002e250e580f5
RJPig <1259950227@qq.com>
2020-05-03 22:07:58 +0800

    设置界面布局

--- clock_demo/js/date.js
+++ clock_demo/js/date.js
@@ -1,12 +1,9 @@
 function showTime() {
   const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDay();
   const hour = date.getHours();
-  const min = date.getMinutes();
-  const second = date.getSeconds();
-  const datetime = hour + ":" + min + ":" + second;
+  const min = date.getMinutes()>9?date.getMinutes():"0"+date.getMinutes();
+  const second = date.getSeconds()>9?date.getSeconds():"0"+date.getSeconds();
+  const datetime = `${hour}:${min}:${second}`;
   const DateTime = document.getElementById('datetime');
   DateTime.innerHTML = datetime
 }
@@ -15,11 +12,23 @@ function showDate() {
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth();
-  const day = date.getDay();
-  const dt = year + "年" + month + "月" + day + "日" 
+  const day = date.getDate();
+  const dt = `${year}年${month+1}月${day}日`;
   const DT = document.getElementById('date');
-  DT.innerHTML = dt
+  DT.innerHTML = dt;
+  
 }
 
 setInterval("showTime()", 1000)
 setInterval("showDate()", 1000)
+
+function btnClick() {
+  document.getElementById("clockset").style.display = 'flex'
+  document.getElementById("clockbody").style.display = 'none'
+
+}
+
+function closeset() {
+  document.getElementById("clockset").style.display = 'none'
+  document.getElementById("clockbody").style.display = 'flex'
+}
