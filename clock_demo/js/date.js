589a26c48fd08223a40adf432057e2b562ca8c35
RJPig <1259950227@qq.com>
2020-05-05 23:30:19 +0800

    就差当闹钟到设定时间时,自动播放音乐这个功能了

--- clock_demo/js/date.js
+++ clock_demo/js/date.js
@@ -1,66 +1,78 @@
-// 获取时分秒
-function showTime() {
-  const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes()>9?date.getMinutes():"0"+date.getMinutes();
-  const second = date.getSeconds()>9?date.getSeconds():"0"+date.getSeconds();
-  const datetime = `${hour}:${min}:${second}`;
-  const DateTime = document.getElementById('datetime');
-  DateTime.innerHTML = datetime
-}
+window.onload = function() {
+  // 获取存放时间和日期的div的id,以便后续innerHTML
+  const time = document.getElementById("time");
+  const DT = document.getElementById("date");
+  // 获取开始按钮id
+  const beginclock = document.getElementById("beginclock");
 
-// 获取年月日
-function showDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDate();
-  const dt = `${year}年${month+1}月${day}日`;
-  const DT = document.getElementById('date');
-  DT.innerHTML = dt;
+  // 获取输入框id
+  const onhour = document.getElementById("hour");
+  const onminute = document.getElementById("minute");
+  const onsecond = document.getElementById("second");
+
+  let h,m,s;
+  // 每一秒刷新一次
+  const timer = setInterval(() => {
+    clock()
+  }, 1000);
+  function clock() {
+    // 调用Date()函数获得当地日期,时间
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
   
+    let hour = date.getHours();
+    let minute = date.getMinutes()>9?date.getMinutes():"0"+date.getMinutes();
+    let second = date.getSeconds()>9?date.getSeconds():"0"+date.getSeconds();
+  
+    // 获取当前时间
+    let T = `${hour}:${minute}:${second}`; 
+    // 获取当前年月日
+    let D = `${year}年${month+1}月${day}日`;
+  
+    // 将获得的日期时间加到div中显示
+    time.innerHTML = T;
+    DT.innerHTML = D;
+    
+    beginclock.onclick = function() {
+      // 获取输入框的值
+      h = onhour.value;
+      m = onminute.value;
+      s = onsecond.value;
+      document.getElementById("clockset").style.display = 'none';
+      document.getElementById("clockbody").style.display = 'flex';
+      console.log(h + ":" + m + ":" + s);
+      alert("设置成功")
+    }
+
+    if((hour == h) && (minute == m) && (second == s)) {
+      // document.getElementById("audio").;
+      alert("叮叮叮")
+    }
+  }
+  // 当页面加载完时调用上面的clock()函数
+  clock()
 }
-// 每隔一秒刷新一次
-setInterval("showTime()", 1000)
-setInterval("showDate()", 1000)
 
 
-
-function btnClick() {
-  document.getElementById("clockset").style.display = 'flex'
-  document.getElementById("clockbody").style.display = 'none'
+// 当点击设置闹钟按钮时,display:none时间显示的界面,打开设置界面
+function openset() {
+  document.getElementById("clockset").style.display = 'flex';
+  document.getElementById("clockbody").style.display = 'none';
 
 }
 
-function closeset() {
-  document.getElementById("clockset").style.display = 'none'
-  document.getElementById("clockbody").style.display = 'flex'
-}
 
 // 获取上传文件地址
-// function show() {
-//   const file = document.getElementById("file");
-//   const audio = document.getElementById("audio");
-//   // audio.src = file.value;
-//   console.log(file.value);
-// }
-
-function select_value() {
-  const hourset = document.getElementById("hour").value;
-  const minuteset = document.getElementById("minute").value
-  const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes()>9?date.getMinutes():"0"+date.getMinutes();
-  const d1 = hour >= hourset && min >= minuteset;
-  const d2 = hour >= hourset;
-  
-  console.log(d1&&d2);
-  
-  // if (d1&&d2) {
-  //   alert("叮叮叮")
-  // }
+function onInputFilePath() {
+  let file = document.getElementById("file").files[0];
+  let url = URL.createObjectURL(file);
+  document.getElementById("audio").src = url;
+  // console.log(url);
 }
-setInterval(select_value,1000)
+
+
 
 
 
